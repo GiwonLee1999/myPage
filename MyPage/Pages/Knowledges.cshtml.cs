429e092ffb7a4baa10253aabbed2cd84c4c01993
Giwon Lee <giwon.lee@outlook.com>
2024-07-03 18:16:04 -0400

    Initial commit

---
+++ MyPage/Pages/Knowledges.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPage.Pages
+{
+    public class KnowledgesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyPage/obj/Release/net8.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿
+document.getElementById('scrollToBottomButton').addEventListener('click', function () {
+    window.scrollTo({
+        top: document.body.scrollHeight, // Scroll to the bottom
+        behavior: 'smooth' // Smooth scrolling
+    });
+});
